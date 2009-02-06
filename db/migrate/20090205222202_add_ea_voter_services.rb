7d57d71d3a62e11664648bbe00dd735d466eacf5
root <matt@neworganizing.com>
2009-02-06 21:25:28 +0000

    added wrapper, map plots street segments in precinct

---
+++ db/migrate/20090205222202_add_ea_voter_services.rb
@@ -0,0 +1,9 @@
+class AddEaVoterServices < ActiveRecord::Migration
+  def self.up
+	:add_column "election_administrations", "voter_services", :text, :limit => 255
+  end
+
+  def self.down
+	:remove_column "election_administrations", "voter_services"
+  end
+end
