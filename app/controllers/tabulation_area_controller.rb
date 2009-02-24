8ef4d10b5f60a072d51fc452280450bae781c158
Matt Brennan <matty.brennan@gmail.com>
2009-02-24 23:48:52 +0000

    showing tabulation areas

---
+++ app/controllers/tabulation_area_controller.rb
@@ -0,0 +1,16 @@
+class LocalityController < ApplicationController
+	layout 'layouts/main'
+
+	def show
+		@locality = Locality.find(params[:id])
+	end
+
+	def index
+		if (params["source"])
+			source = Source.find(params["source"])
+			@localities = source.localities
+		else
+			@localities = Locality.find(:all)
+		end
+	end
+end
