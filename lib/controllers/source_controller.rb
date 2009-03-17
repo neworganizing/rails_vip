2ca499664a7a71db9586f512c4544dd6d3136fde
Matt Brennan <matty.brennan@gmail.com>
2009-03-17 17:13:02 +0000

    move controllers to the right spot

--- lib/controllers/source_controller.rb
+++
@@ -1,8 +0,0 @@
-class SourceController < ApplicationController
-	def index
-		@sources = Source.find(:all, :conditions => "active = 1")
-	end
-	def show 
-		@source = Source.find(params[:id])
-	end
-end
